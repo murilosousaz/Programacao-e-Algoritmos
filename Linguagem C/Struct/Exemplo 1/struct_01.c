ac9bc53dd7cd77a8680dc2003b5d3422bbe56dfb
Murilo Sousa <murilosz085@gmail.com>
2025-10-16 21:56:31 -0300

    struct

--- Linguagem C/Struct/Exemplo 1/struct_01.c
+++ Linguagem C/Struct/Exemplo 1/struct_01.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct cadastro{
+    char nome[50];
+    int idade;
+    char rua[50];
+    int numero;
+};
 int main(){
-    
+    struct cadastro c;
+    printf("Insira o seu nome: ");
+    gets(c.nome);
+    printf("Insira sua idade: ");
+    scanf("%d", &c.idade);
+    printf("Insira a sua rua: ");
+    gets(c.rua);
+    printf("Insira seu numero: ");
+    scanf("%d", &c.numero);
     return 0;
 }
