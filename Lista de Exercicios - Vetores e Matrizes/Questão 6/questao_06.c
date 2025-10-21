6a88ab1b31987eb4324c7d0b0ed9d932f0dc04b3
Murilo Sousa <murilosz085@gmail.com>
2025-10-21 09:28:03 -0300

    add res questao 06

--- Lista de Exercicios - Vetores e Matrizes/Questão 6/questao_06.c
+++ Lista de Exercicios - Vetores e Matrizes/Questão 6/questao_06.c
@@ -4,3 +4,17 @@ por um vetor de números inteiros que indicavam o id de cada livro. Então, ela 
 sua ajuda para fazer um algoritmo que receba o tamanho de um vetor, depois receba 
 os ids dos livros, e no final receba o id de um livro a ser pesquisado. O algoritmo deve 
 verificar se o livro está disponível e, se sim, apresentar o índice.*/
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+    int lista[10];
+    int i, id_livros = 0;
+
+    for(i = 0; i < 10; i++){
+        id_livros = lista[id_livros + 1];
+    }
+    printf("O id do livro eh: %d", lista[id_livros]);
+
+    return 0;
+}
