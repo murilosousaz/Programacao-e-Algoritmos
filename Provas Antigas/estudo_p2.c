f266a0ca201b0e01db588a21c05eba90f0e426a2
Murilo Sousa <murilosz085@gmail.com>
2025-10-22 20:13:45 -0300

    add res questao 2 prova antiga

--- Provas Antigas/estudo_p2.c
+++ Provas Antigas/estudo_p2.c
@@ -2,6 +2,34 @@
 #include <stdlib.h>
 
 int main(){
+    float mat[4][7];
+    int i, j, k, aux;
+    for(i = 0; i < 4; i++){
+        printf("Semana: ");
+        scanf("%f", &mat[i][0]);
+    }
+    for(j = 0; j < 7; j++){
+            printf("Consumo: ");
+            scanf("%f", &mat[0][j]);
+    }
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 7; j++){
+        printf("%f", mat[i][j]);
+        }
+    }
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 7; j++){
+            aux = mat[i][j];
+            mat[i][j] = mat[i++][j++];
+            mat[i++][j++] = aux;
+        }
+    }
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 7; j++){
+        printf("%f", mat[i][j]);
+        }
+    }
+
 
     return 0;
 }
