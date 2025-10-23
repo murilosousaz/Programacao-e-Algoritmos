1bcd9af1a5b55d3a4d31673cef1028d4eb6c803d
Murilo Sousa <murilosz085@gmail.com>
2025-10-22 21:28:50 -0300

    revisao da revisao // questao 02

--- Provas Antigas/estudo_p2.c
+++ Provas Antigas/estudo_p2.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
     float mat[4][7];
-    int i, j, k, aux;
-    for(i = 0; i < 4; i++){
-        printf("Semana: ");
+    int i, j;
+    float aux;
+
+    for(i = 0; i < 4; i++) {
         scanf("%f", &mat[i][0]);
     }
+    
     for(j = 0; j < 7; j++){
-            printf("Consumo: ");
-            scanf("%f", &mat[0][j]);
+        scanf("%f", &mat[0][j]);
     }
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 7; j++){
-        printf("%.2f \n", mat[i][j]);
+
+    printf("\nMatriz:\n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 7; j++) {
+            printf("%.2f ", mat[i][j]);
         }
+        printf("\n");
     }
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 7 - 1; j++){
-            if(mat[i][j] > mat[i++][j++]){
-                aux = mat[i][j];
-                mat[i][j] = mat[i++][j++];
-                mat[i++][j++] = aux;
-                printf("%.2f \n", mat[i][j]);
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 7; j++) {
+            for(int k = 0; k < 4; k++) {
+                for(int l = 0; l < 7; l++) {
+                    if(mat[i][j] > mat[k][l]) {
+                        aux = mat[i][j];
+                        mat[i][j] = mat[k][l];
+                        mat[k][l] = aux;
+                    }
+                }
             }
         }
+    }
+
+    // Print sorted matrix
+    printf("\nMatriz decrescente: \n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 7; j++) {
+            printf("%.2f ", mat[i][j]);
+        }
+        printf("\n");
     }
     
     return 0;
