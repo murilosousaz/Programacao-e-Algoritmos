b17fb00a2ace1e678077301c3ca58957ee1c7ee9
murilosousaz <murilosz085@gmail.com>
2025-11-27 21:16:57 -0300

    teste

--- Lista de Exercícios - Arquivos/Questão 2/q_02_arquivo.c
+++ Lista de Exercícios - Arquivos/Questão 2/q_02_arquivo.c
@@ -2,3 +2,21 @@
 arquivos .txt, seguindo o formato "NOME: JOÃO NOTA: 8", com um aluno por linha. Ela 
 solicitou a criação de um programa que lendo um arquivo .txt qualquer no formato dela 
 exiba na tela o nome e a nota do aluno que obteve a maior pontuação.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    FILE *arq;
+    arq = fopen("notas.txt", "r");
+    if(arq == NULL){
+        printf("Não foi possivel ler o arquivo");
+        system("pause");
+        return 1;
+    }
+
+    
+    fclose(arq);
+
+    return 0;
+}
